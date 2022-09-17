fda6d8a513cb141972b6c934147c162469316f07
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-09-17 20:56:41 +0430

    the initialization of the authentication service

---
+++ servers/auth/README.md
@@ -0,0 +1,1 @@
+RabbitMQ dashboard: http://localhost:15672/#/queues/%2F/user-messages
---
+++ servers/auth/src/app.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: process.env.RABBITMQ_QUEUE,
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_NAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
