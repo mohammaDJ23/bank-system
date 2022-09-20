7cb54843989c103252aa92fc99863a2b53ac5de6
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-09-20 22:55:22 +0430

    adding the serializer interceptor

--- servers/auth/src/app.controller.ts
+++ servers/auth/src/app.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SignupDto } from './dtos/signup.dto';
 import { UserDto } from './dtos/user.dto';
+import { Serializer } from './interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('signup')
+  @Serializer(UserDto)
   signup(@Body() body: SignupDto): Promise<UserDto | void> {
     return this.appService.signup(body);
   }
