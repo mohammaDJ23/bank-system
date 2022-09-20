18a39f70d90f712c98f2365d5944ce03dc085cc4
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-09-20 17:41:52 +0430

    adding the logics for the creation of the user

--- servers/user/src/app.service.ts
+++ servers/user/src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './entities/user.entity';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class AppService {
@@ -10,7 +11,25 @@ export class AppService {
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
-  createUser(payload: CreateUserDto): Promise<CreateUserDto> {
-    return new Promise((resolve) => resolve(payload));
+  async createUser(payload: CreateUserDto): Promise<CreateUserDto> {
+    const user = await this.userRepository.findOne({
+      where: { email: payload.email },
+    });
+
+    if (user) {
+      throw new ConflictException('The user already exist.');
+    }
+
+    const hashedPassword = await hash(payload.password, 10);
+
+    const createdUser = this.userRepository.create({
+      firstName: payload.firstName,
+      lastName: payload.lastName,
+      email: payload.email,
+      password: hashedPassword,
+      phone: payload.phone,
+    });
+
+    return this.userRepository.save(createdUser);
   }
 }
