845d404821423320d26308ad3a87e8e4032ee440
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-09-23 21:30:56 +0330

    cleaning up the user service

--- servers/user/src/app.service.ts
+++ servers/user/src/app.service.ts
@@ -7,11 +7,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './entities/user.entity';
-import { compare, hash } from 'bcrypt';
+import { hash } from 'bcrypt';
 import { RpcException } from '@nestjs/microservices';
 import { UserDto } from './dtos/user-dto';
-import { ValidateUserDto } from './dtos/validate-user.dto';
-import { ResetPasswordDto } from './dtos/reset-password.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { GetAllDto } from './dtos/get-all.dto';
 
@@ -84,50 +82,3 @@ export class AppService {
     });
   }
 }
-
-// async createUser(payload: CreateUserDto): Promise<UserDto> {
-//   let user = await this.findByEmail(payload.email);
-
-//   if (user)
-//     throw new RpcException(new ConflictException('The user already exist.'));
-
-//   payload.password = await hash(payload.password, 10);
-//   user = this.userRepository.create(payload);
-//   return this.userRepository.save(user);
-// }
-
-// async validateUser(payload: ValidateUserDto): Promise<UserDto> {
-//   const user = await this.findByEmail(payload.email);
-
-//   if (!user)
-//     throw new RpcException(
-//       new NotFoundException('Could not found the email.'),
-//     );
-
-//   const isPasswordsEqual = await compare(payload.password, user.password);
-
-//   if (!isPasswordsEqual)
-//     throw new RpcException(new NotFoundException(`The password is wrong.`));
-
-//   return Object.assign(user, { password: payload.password });
-// }
-
-// findById(id: number): Promise<UserDto> {
-//   return this.userRepository.findOne({ where: { id } });
-// }
-
-// findByEmail(email: string): Promise<UserDto> {
-//   return this.userRepository.findOne({ where: { email } });
-// }
-
-// async resetPassword(payload: ResetPasswordDto): Promise<UserDto> {
-//   const user = await this.findById(payload.userId);
-
-//   if (!user)
-//     throw new RpcException(
-//       new NotFoundException('Cound not found the user.'),
-//     );
-
-//   user.password = await hash(payload.password, 10);
-//   return this.userRepository.save(user);
-// }
