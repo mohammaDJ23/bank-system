1ab96cdc7e9fe8df61cd10a16684303c0cc5443a
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-09-29 10:31:29 +0330

    adding some configuration to the service

--- servers/request-tracker/src/app.module.ts
+++ servers/request-tracker/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule } from '@nestjs/microservices';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomNamingStrategy } from './strategies/naming.strategy';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([]),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        namingStrategy: new CustomNamingStrategy(),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+    TypeOrmModule.forFeature([]),
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ servers/request-tracker/src/types/rabbitmq.ts
@@ -0,0 +1,7 @@
+export enum RabbitMqServices {
+  REQUSET_TRACKER = 'REQUEST_TRACKER_SERVICE',
+}
+
+export enum RabbitMqQueue {
+  REQUEST_TRACKER = 'request-tracker-queue',
+}
