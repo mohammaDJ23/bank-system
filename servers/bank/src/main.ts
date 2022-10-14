011b0ffcf37c700747209d3280252ca2d7bdf4a3
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-10-14 11:44:52 +0330

    using env port

--- servers/bank/src/main.ts
+++ servers/bank/src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
-  await app.listen(3003);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
--- servers/user/src/main.ts
+++ servers/user/src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
-  await app.listen(3002);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
