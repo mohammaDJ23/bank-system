8d300a9d59dde54db3812abb09ef3c45a3b2dc4a
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-10-26 00:31:06 +0330

    adding swagger decorators to all route

--- servers/user/src/controllers/gateway.controller.ts
+++ servers/user/src/controllers/gateway.controller.ts
@@ -99,6 +99,11 @@ export class GatewayController {
   @HttpCode(HttpStatus.OK)
   @UseGuards(AdminAuthGuard)
   @ListSerializer(UserDto)
+  @ApiBody({ type: FindAllDto })
+  @ApiBearerAuth()
+  @ApiResponse({ status: HttpStatus.OK, type: UserDto, isArray: true })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, type: ErrorDto })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, type: ErrorDto })
   findAll(@Body() body: FindAllDto): Promise<[User[], number]> {
     return this.userService.findAll(body);
   }
--- servers/user/src/dtos/list.dto.ts
+++ servers/user/src/dtos/list.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 
 export class ListDto {
   @IsNumber()
+  @ApiProperty()
   page: number;
 
   @IsNumber()
+  @ApiProperty()
   take: number;
 }
