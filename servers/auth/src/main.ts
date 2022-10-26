51ae9dd7c958559a650b1b2327e721cff80910f4
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-10-26 16:34:57 +0330

    adding swagger initialization code

--- servers/auth/src/main.ts
+++ servers/auth/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { RabbitMqQueue } from './types/rabbitmq';
+import { swagger } from './libs/swagger';
 
 require('dotenv').config();
 
@@ -20,6 +21,7 @@ async function bootstrap() {
     },
   });
 
+  swagger(app);
   await app.startAllMicroservices();
   await app.listen(process.env.PORT);
 }
