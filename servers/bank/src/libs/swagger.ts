50557709e7fb967b86d536d26d0272a58ced7622
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-10-26 16:55:15 +0330

    revising some info around swagger

--- servers/bank/src/libs/swagger.ts
+++ servers/bank/src/libs/swagger.ts
@@ -3,12 +3,12 @@ import { INestApplication } from '@nestjs/common';
 
 export function swagger(app: INestApplication): void {
   const config = new DocumentBuilder()
-    .setTitle('Auth apis')
-    .setDescription('The apis of the auth service')
+    .setTitle('Bank apis')
+    .setDescription('The apis of the bank service')
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('auth', app, document);
+  SwaggerModule.setup('bank', app, document);
 }
