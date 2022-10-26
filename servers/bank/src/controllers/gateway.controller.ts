e26589a167d2c31ee8c6fd4b48afb7323277a9c2
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-10-26 18:51:41 +0330

    setting up swagger decoratores to bills route

--- servers/bank/src/controllers/gateway.controller.ts
+++ servers/bank/src/controllers/gateway.controller.ts
@@ -180,6 +180,11 @@ export class GatewayController {
   @Get('bills')
   @HttpCode(HttpStatus.OK)
   @ListSerializer(BillDto)
+  @ApiBody({ type: ListDto })
+  @ApiBearerAuth()
+  @ApiResponse({ status: HttpStatus.OK, type: BillDto, isArray: true })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, type: ErrorDto })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, type: ErrorDto })
   findAllBills(
     @Body() body: ListDto,
     @CurrentUser() user: User,
