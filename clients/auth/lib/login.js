65aab7d6059cbd82f134e9c2c65e3ee4a3f28642
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-02 11:26:09 +0330

    adding a basics of the fomr

---
+++ clients/auth/lib/login.js
@@ -0,0 +1,13 @@
+import { Form } from './formConstructor';
+
+export class Login extends Form {
+  email = '';
+
+  password = '';
+}
+
+export class AdminLogin extends Form {
+  email = '';
+
+  password = '';
+}
