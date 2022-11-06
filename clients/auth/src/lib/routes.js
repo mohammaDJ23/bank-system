a86e020cfe7ced893d02845532d27b1862656c0d
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-06 17:39:02 +0330

    adding notfound routes

--- clients/auth/src/lib/routes.js
+++ clients/auth/src/lib/routes.js
@@ -1,3 +1,6 @@
 import Login from '../pages/Login.vue';
 
-export const routes = [{ path: '/auth/login', name: 'Login', component: Login }];
+export const routes = [
+  { path: '/auth/login', name: 'Login', component: Login },
+  { path: '/:CatchAll(.*)*', redirect: '/auth/login' },
+];
