272a89285ff9299aec2d2fd0d7e692c59ed3d084
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-06 20:24:18 +0330

    the basic implementation of the reset password

--- clients/auth/src/lib/routes.js
+++ clients/auth/src/lib/routes.js
@@ -1,6 +1,8 @@
 import Login from '../pages/Login.vue';
+import ResetPassword from '../pages/ResetPassword.vue';
 
 export const routes = [
   { path: '/auth/login', name: 'Login', component: Login },
-  { path: '/:CatchAll(.*)*', redirect: '/auth/login' },
+  { path: '/auth/reset-password', name: 'ResetPassword', component: ResetPassword },
+  { path: '/:catchAll(.*)*', redirect: '/auth/login' },
 ];
