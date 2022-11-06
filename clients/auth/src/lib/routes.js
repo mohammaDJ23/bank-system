3afe84843f73d373b8c85747b891161fe2d6b191
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-06 17:29:19 +0330

    removing the admin auth page

--- clients/auth/src/lib/routes.js
+++ clients/auth/src/lib/routes.js
@@ -1,7 +1,3 @@
 import Login from '../pages/Login.vue';
-import AdminLogin from '../pages/AdminLogin.vue';
 
-export const routes = [
-  { path: '/auth/login', name: 'Login', component: Login },
-  { path: '/auth/login/admin', name: 'AdminLogin', component: AdminLogin },
-];
+export const routes = [{ path: '/auth/login', name: 'Login', component: Login }];
