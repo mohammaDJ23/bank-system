488aec3832304b29e2ca876d365f701c7cfb7348
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-06 21:11:03 +0330

    adding getters

--- clients/auth/src/store/getters.js
+++ clients/auth/src/store/getters.js
@@ -1,1 +1,17 @@
-export const getters = {};
+export const getters = {
+  getForms: function (state) {
+    return state;
+  },
+
+  getForm: function (state) {
+    return function (instance) {
+      return state[instance.name];
+    };
+  },
+
+  getInput: function (state) {
+    return function (instance, inputName) {
+      return state[instance.name][inputName];
+    };
+  },
+};
