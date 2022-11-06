6e72b1363adb3bae6b21520dcf271939db4466ef
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-06 17:03:36 +0330

    spliting the form component

---
+++ clients/auth/src/components/Form.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header text-center">
+        <span
+          ><b>{{ formTitle }}</b></span
+        >
+      </div>
+    </template>
+
+    <form @keyup.enter="onSubmit">
+      <div class="d-flex flex-column gap-3 justify-content-center align-items-center">
+        <slot />
+
+        <div class="w-50">
+          <el-button class="w-100" type="primary" @click="onSubmit">{{ buttonTitle }}</el-button>
+        </div>
+      </div>
+    </form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  props: { formTitle: String, buttonTitle: { type: String, value: 'Send' } },
+
+  methods: {
+    onSubmit: function (event) {
+      console.log(event);
+    },
+  },
+};
+</script>
