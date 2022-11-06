28ea331c94df10bb16529a3969ae3a05de940d58
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-06 20:28:13 +0330

    the basic implementation of the forgot password

--- clients/auth/src/lib/routes.js
+++ clients/auth/src/lib/routes.js
@@ -1,8 +1,10 @@
 import Login from '../pages/Login.vue';
 import ResetPassword from '../pages/ResetPassword.vue';
+import ForgotPassword from '../pages/ForgotPassword.vue';
 
 export const routes = [
   { path: '/auth/login', name: 'Login', component: Login },
   { path: '/auth/reset-password', name: 'ResetPassword', component: ResetPassword },
+  { path: '/auth/forgot-password', name: 'ForgotPassword', component: ForgotPassword },
   { path: '/:catchAll(.*)*', redirect: '/auth/login' },
 ];
