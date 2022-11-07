700e0495279d460623e0786a2128a4bd3f478754
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-07 16:48:28 +0330

    adding changeForm mutation

--- clients/auth/src/store/mutations.js
+++ clients/auth/src/store/mutations.js
@@ -2,4 +2,13 @@ export const mutations = {
   setForms: function (state, forms) {
     for (let instance of forms) state[instance.name] = new instance();
   },
+
+  changeForm: function (state, { instance, inputName, value }) {
+    const form = state[instance.name];
+
+    if (!form) throw new Error('Invalid instance.');
+    if (!form[inputName]) throw new Error('Invalid input.');
+
+    form[inputName] = value;
+  },
 };
