8e84bac6d6b5434fbc2315f7d18a26a949607143
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-07 16:49:55 +0330

    adding changeForm action

--- clients/auth/src/store/actions.js
+++ clients/auth/src/store/actions.js
@@ -2,4 +2,8 @@ export const actions = {
   setForms: function (context, forms) {
     context.commit('setForms', forms);
   },
+
+  changeForm: function (context, payload) {
+    context.commit('changeForm', payload);
+  },
 };
