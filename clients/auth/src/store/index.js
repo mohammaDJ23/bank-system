5898142e5e120c98c73ca3ce160f428d50c9a458
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-14 08:46:26 +0330

    refactoring the entire state

--- clients/auth/src/store/index.js
+++ clients/auth/src/store/index.js
@@ -1,9 +1,4 @@
 import { createStore } from 'vuex';
-import { state } from './state';
-import { actions } from './actions';
-import { mutations } from './mutations';
-import { getters } from './getters';
-
-const authModule = { state, actions, mutations, getters };
+import { authModule } from './modules/auth';
 
 export const store = createStore({ modules: { authModule } });
