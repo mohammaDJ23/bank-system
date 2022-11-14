ec0bdacc91813a40e828edd85e5009c8dbf1f8ba
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-14 07:54:20 +0330

    adding a logic to check if the user has the token

--- clients/auth/src/lib/router/routes.js
+++ clients/auth/src/lib/router/routes.js
@@ -4,7 +4,15 @@ import ForgotPassword from '../../pages/ForgotPassword.vue';
 
 export const routes = [
   { path: '/auth/login', name: 'Login', component: Login },
-  { path: '/auth/reset-password', name: 'ResetPassword', component: ResetPassword },
+  {
+    path: '/auth/reset-password',
+    name: 'ResetPassword',
+    component: ResetPassword,
+    beforeEnter: (to, from, next) => {
+      if (!('token' in to.query)) next(from.path);
+      else next();
+    },
+  },
   { path: '/auth/forgot-password', name: 'ForgotPassword', component: ForgotPassword },
   { path: '/:catchAll(.*)*', redirect: '/auth/login' },
 ];
