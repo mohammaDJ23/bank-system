57a84f4a70e8e399b583813c6a9e5580628b7c6b
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-14 09:50:45 +0330

    adding getters

--- clients/auth/src/store/modules/requestProcess/getters.js
+++ clients/auth/src/store/modules/requestProcess/getters.js
@@ -1,1 +1,21 @@
-export const getters = {};
+export const getters = {
+  getLoadings(state) {
+    return state.loadings;
+  },
+
+  getErrors(state) {
+    return state.errors;
+  },
+
+  getLoading(state) {
+    return function (name) {
+      return state.loadings[name];
+    };
+  },
+
+  getError(state) {
+    return function (name) {
+      return state.errors[name];
+    };
+  },
+};
