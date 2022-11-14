31589adf36e2263f7b8fc9e7d7f8500ba455869b
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-14 13:49:36 +0330

    removing the error mutation

--- clients/auth/src/store/modules/requestProcess/mutations.js
+++ clients/auth/src/store/modules/requestProcess/mutations.js
@@ -1,21 +1,11 @@
-function cleanUpState(state, payload) {
-  setTimeout(() => {
-    delete state.loadings[payload];
-    delete state.errors[payload];
-  }, 5000);
-}
-
 export const mutations = {
   loading(state, payload) {
     state.loadings = Object.assign(state.loadings, payload);
   },
 
   success(state, payload) {
-    cleanUpState(state, payload);
-  },
-
-  error(state, payload) {
-    state.errors = Object.assign(state.errors, payload);
-    cleanUpState(state, payload);
+    setTimeout(() => {
+      delete state.loadings[payload];
+    }, 5000);
   },
 };
