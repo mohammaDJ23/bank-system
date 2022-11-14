596269f6c9a3c58df168c2fccdd84c3e75c3be35
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-14 09:20:57 +0330

    adding the request logic

--- clients/auth/src/store/modules/requestProcess/actions.js
+++ clients/auth/src/store/modules/requestProcess/actions.js
@@ -1,5 +1,24 @@
+import { ResetApi, apis } from '../../../apis';
+
 export const actions = {
   async request(context, formSchema) {
-    console.log(formSchema);
+    const formConstructorName = formSchema?.constructor?.name || '';
+
+    try {
+      context.commit('loading', { [formConstructorName]: true });
+
+      if (typeof formSchema.beforeSubmit === 'function') formSchema.beforeSubmit(context);
+
+      const restApi = new ResetApi();
+      const res = await restApi.build(apis[formConstructorName](formSchema));
+
+      if (typeof formSchema.afterSubmit === 'function') formSchema.afterSubmit(context, res);
+
+      context.commit('success', formConstructorName);
+    } catch (error) {
+      context.commit('error', { [formConstructorName]: error });
+    } finally {
+      context.commit('loading', { [formConstructorName]: false });
+    }
   },
 };
