61aa5823a4a0ccf608a7365a07f24e65dbea1897
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-14 09:46:19 +0330

    adding the request processing mutation

--- clients/auth/src/store/modules/requestProcess/mutations.js
+++ clients/auth/src/store/modules/requestProcess/mutations.js
@@ -1,1 +1,21 @@
-export const mutations = {};
+function cleanUpState(state, payload) {
+  setTimeout(() => {
+    delete state.loadings[payload];
+    delete state.errors[payload];
+  }, 5000);
+}
+
+export const mutations = {
+  loading(state, payload) {
+    state.loadings = Object.assign(state.loadings, payload);
+  },
+
+  success(state, payload) {
+    cleanUpState(state, payload);
+  },
+
+  error(state, payload) {
+    state.errors = Object.assign(state.errors, payload);
+    cleanUpState(state, payload);
+  },
+};
