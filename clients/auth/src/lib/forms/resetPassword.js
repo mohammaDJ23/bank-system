982b9aac1a1e5212a57bbfc140112379d7c9c337
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-15 15:30:56 +0330

    redirecting the user to the auth/login after submition the ResetPassword

--- clients/auth/src/lib/forms/resetPassword.js
+++ clients/auth/src/lib/forms/resetPassword.js
@@ -1,7 +1,13 @@
 import { Form } from './formConstructor';
+import { isMicroFrontEnd } from '../../lib';
+import { router } from '../../main';
 
 export class ResetPassword extends Form {
   password = '';
 
   confirmedPassword = '';
+
+  afterSubmit(context, res) {
+    router.push('/auth/login');
+  }
 }
