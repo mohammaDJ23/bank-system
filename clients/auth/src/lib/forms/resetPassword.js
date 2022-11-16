3a8b6c812cf46b8fc1f10e03a2fa1cd783c92695
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-16 09:26:27 +0330

    adding token to the form

--- clients/auth/src/lib/forms/resetPassword.js
+++ clients/auth/src/lib/forms/resetPassword.js
@@ -8,6 +8,12 @@ export class ResetPassword extends Form {
 
   token = '';
 
+  beforeSubmit(context) {
+    const params = new URLSearchParams(window.location.search);
+
+    if (params.has('token')) this.token = params.get('token');
+  }
+
   afterSubmit(context, res) {
     router.push('/auth/login');
   }
