d9637bdbd17da5286cbab10f4b9d7610b6efc96d
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-16 13:03:17 +0330

    doing some configuration around webpack

--- clients/container/config/webpack.dev.js
+++ clients/container/config/webpack.dev.js
@@ -1,7 +1,12 @@
 const { merge } = require('webpack-merge');
+const path = require('path');
 const { ModuleFederationPlugin } = require('webpack').container;
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
+const webpack = require('webpack');
+const dotenv = require('dotenv').config({
+  path: path.resolve(__dirname, '../.env.development'),
+});
 
 module.exports = merge(commonConfig, {
   mode: 'development',
@@ -14,6 +19,9 @@ module.exports = merge(commonConfig, {
       exposes: {},
       remotes: { auth: 'auth@http://localhost:3005/remoteEntry.js' },
       shared: packageJson.dependencies,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(dotenv.parsed),
     }),
   ],
 });
