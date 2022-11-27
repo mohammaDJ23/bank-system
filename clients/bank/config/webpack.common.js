b222d10c73cb85fb1dc4195925a465465e15bd14
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-27 10:21:26 +0330

    using bootstrap

--- clients/bank/config/webpack.common.js
+++ clients/bank/config/webpack.common.js
@@ -33,7 +33,7 @@ module.exports = {
       },
       {
         test: /\.scss|\.css$/,
-        use: ['style-loader', { loader: 'css-loader', options: { modules: true } }, 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
