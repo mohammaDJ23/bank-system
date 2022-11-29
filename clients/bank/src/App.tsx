4c85ce9acf7accf8f9bc86ba5ab6af124bb6aaeb
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-11-29 10:07:22 +0330

    adding the routes

--- clients/bank/src/App.tsx
+++ clients/bank/src/App.tsx
@@ -1,9 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import 'element-theme-default';
 
 function App() {
-  return <div>bank</div>;
+  return (
+    <BrowserRouter basename="bank">
+      <Routes>
+        <Route path="create-user" element={<div>create user</div>} />
+        <Route path="update-user" element={<div>update user</div>} />
+        <Route path="users" element={<div>update user</div>} />
+        <Route path="create-bill" element={<div>create bill</div>} />
+        <Route path="update-bill" element={<div>update bill</div>} />
+        <Route path="bills" element={<div>update bill</div>} />
+        <Route path="users/:id" element={<div>update user</div>} />
+        <Route path="bills/:id" element={<div>update bill</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
