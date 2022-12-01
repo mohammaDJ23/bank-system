300f6245931a5baea4a35b2c9b549a3ef051c1d6
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-12-01 19:05:40 +0330

    removing the pathname and using it into the route and adding a route to redirect to the initial path

--- clients/bank/src/App.tsx
+++ clients/bank/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import { Routes, Route, unstable_HistoryRouter as HistoryRouter, Navigate } from 'react-router-dom';
 import { FC } from 'react';
 import { Bank } from './types';
 
@@ -9,16 +9,17 @@ import 'element-theme-default';
 const App: FC<Bank.AppImportation> = props => {
   return (
     /**@ts-ignore */
-    <HistoryRouter history={props.history} basename="bank">
+    <HistoryRouter history={props.history}>
       <Routes>
-        <Route path="create-user" element={<div>create user</div>} />
-        <Route path="update-user" element={<div>update user</div>} />
-        <Route path="users" element={<div>update user</div>} />
-        <Route path="create-bill" element={<div>create bill</div>} />
-        <Route path="update-bill" element={<div>update bill</div>} />
-        <Route path="bills" element={<div>update bill</div>} />
-        <Route path="users/:id" element={<div>update user</div>} />
-        <Route path="bills/:id" element={<div>update bill</div>} />
+        <Route path="bank/create-user" element={<div>create user</div>} />
+        <Route path="bank/update-user" element={<div>update user</div>} />
+        <Route path="bank/users" element={<div>update user</div>} />
+        <Route path="bank/create-bill" element={<div>create bill</div>} />
+        <Route path="bank/update-bill" element={<div>update bill</div>} />
+        <Route path="bank/bills" element={<div>update bill</div>} />
+        <Route path="bank/users/:id" element={<div>update user</div>} />
+        <Route path="bank/bills/:id" element={<div>update bill</div>} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </HistoryRouter>
   );
