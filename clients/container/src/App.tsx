d79255724cb3142583383650fb3404ee9b20ace5
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-12-01 16:15:55 +0330

    adding bank routes

--- clients/container/src/App.tsx
+++ clients/container/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Auth from './components/Auth';
 import Bank from './components/Bank';
 import { isUserAuthenticated } from './lib';
@@ -7,15 +7,21 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {!isUserAuthenticated() && (
-          <Route path="auth">
-            <Route path="*" element={<Auth />} />
-          </Route>
-        )}
+        <>
+          {!isUserAuthenticated() && (
+            <Route path="auth">
+              <Route path="*" element={<Auth />} />
+            </Route>
+          )}
 
-        <Route path="bank">
-          <Route path="*" element={<Bank />} />
-        </Route>
+          {isUserAuthenticated() && (
+            <Route path="bank">
+              <Route path="*" element={<Bank />} />
+            </Route>
+          )}
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </>
       </Routes>
     </BrowserRouter>
   );
