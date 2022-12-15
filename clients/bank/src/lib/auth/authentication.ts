6b5df732bddb6cc0e4db87efbdaaa50367517719
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-12-15 03:09:54 -0800

    adding auth file to the user token

---
+++ clients/bank/src/lib/auth/authentication.ts
@@ -0,0 +1,34 @@
+import { decodeToken } from 'react-jwt';
+import { LocalStorage } from '../';
+
+export enum UserRoles {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+export interface TokenInfo {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: UserRoles;
+  expiration: string;
+}
+
+export function getToken(): string {
+  const token = LocalStorage.getItem<string>('access_token');
+  const tokenExpiration = LocalStorage.getItem<number>('access_token_expiration');
+
+  if (token && tokenExpiration)
+    if (new Date().getTime() < new Date(tokenExpiration).getTime()) return token;
+
+  return '';
+}
+
+export function getTokenInfo() {
+  return decodeToken<TokenInfo>(getToken());
+}
+
+export function isUserAuthenticated() {
+  return !!getToken();
+}
