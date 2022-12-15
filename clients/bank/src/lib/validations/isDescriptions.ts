caa78526720435faf26b2ae9550191562bba8dc9
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-12-15 10:22:33 -0800

    limiting the description

--- clients/bank/src/lib/validations/isDescriptions.ts
+++ clients/bank/src/lib/validations/isDescriptions.ts
@@ -4,6 +4,6 @@ export function isDescription(
   callback: (error?: Error) => void
 ): void {
   if (value.length < 3) callback(new Error('Invalid description'));
-  else if (value.length > 100) callback(new Error('The description is too long'));
+  else if (value.length > 500) callback(new Error('The description is too long'));
   else callback();
 }
