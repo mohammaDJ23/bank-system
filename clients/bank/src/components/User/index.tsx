e94a0afeb2079696d978866dafd11e1bad153ada
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-12-22 07:54:12 +0330

    adding user details

--- clients/bank/src/components/User/index.tsx
+++ clients/bank/src/components/User/index.tsx
@@ -1,5 +1,91 @@
+import { useState } from 'react';
+import moment from 'moment';
+import { Box, Typography, IconButton, Menu, MenuItem } from '@mui/material';
+import { MoreVert } from '@mui/icons-material';
+import DefaultContainer from '../../layout/DefaultContainer';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'element-react';
+
 const UserContent = () => {
-  return <div></div>;
+  const user = {
+    id: 1,
+    firstName: 'Mohammad',
+    lastName: 'Nowresideh',
+    email: 'mohammad.nowresideh@gmail.com',
+    phone: '09174163042',
+    role: 'admin',
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  };
+  const options = [{ label: 'Update', path: `/bank/update-user/${user.id}` }];
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const navigate = useNavigate();
+
+  function onMenuOpen(event: React.MouseEvent<HTMLElement>) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function onMenuClose() {
+    setAnchorEl(null);
+  }
+
+  function onMenuClick(option: typeof options[number]) {
+    return function () {
+      onMenuClose();
+      navigate(option.path);
+    };
+  }
+
+  return (
+    <DefaultContainer>
+      <Box width="100%" display="flex" flexDirection="column" alignItems="start" gap="8px">
+        <Box
+          width="100%"
+          mb="15px"
+          display="flex"
+          gap="8px"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography fontWeight="700" fontSize="16px">
+            {user.firstName} {user.lastName}
+          </Typography>
+          <IconButton onClick={onMenuOpen}>
+            <MoreVert />
+          </IconButton>
+          <Menu anchorEl={anchorEl} open={open} onClick={onMenuClose}>
+            {options.map(option => (
+              <MenuItem key={option.path} onClick={onMenuClick(option)}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+        <Typography fontSize="12px" color="">
+          email: {user.email}
+        </Typography>
+        <Typography fontSize="12px" color="">
+          phone: {user.phone}
+        </Typography>
+        <Typography fontSize="12px" color="">
+          role: {user.role}
+        </Typography>
+        <Typography fontSize="12px" color="">
+          created at: {moment(user.createdAt).format('LLLL')}
+        </Typography>
+        {new Date(user.updatedAt) > new Date(user.createdAt) && (
+          <Typography fontSize="12px" color="">
+            last update: {moment(user.updatedAt).format('LLLL')}
+          </Typography>
+        )}
+        <Box mt="30px">
+          {/**@ts-ignore */}
+          <Button type="danger">Delete account</Button>
+        </Box>
+      </Box>
+    </DefaultContainer>
+  );
 };
 
 export default UserContent;
