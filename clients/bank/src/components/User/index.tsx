00fb23d4cda244fafd6f99f7cd9f04d93b9a70ce
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-12-22 08:01:57 +0330

    using a modal for deleting the user account

--- clients/bank/src/components/User/index.tsx
+++ clients/bank/src/components/User/index.tsx
@@ -5,6 +5,9 @@ import { MoreVert } from '@mui/icons-material';
 import DefaultContainer from '../../layout/DefaultContainer';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'element-react';
+import { useAction, useSelector } from '../../hooks';
+import Modal from '../Modal';
+import { ModalNames } from '../../store';
 
 const UserContent = () => {
   const user = {
@@ -21,6 +24,8 @@ const UserContent = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const navigate = useNavigate();
+  const { showModal, hideModal } = useAction();
+  const { modals } = useSelector();
 
   function onMenuOpen(event: React.MouseEvent<HTMLElement>) {
     setAnchorEl(event.currentTarget);
@@ -37,54 +42,69 @@ const UserContent = () => {
     };
   }
 
+  function onDeleteAccount() {
+    showModal(ModalNames.CONFIRMATION);
+  }
+
   return (
-    <DefaultContainer>
-      <Box width="100%" display="flex" flexDirection="column" alignItems="start" gap="8px">
-        <Box
-          width="100%"
-          mb="15px"
-          display="flex"
-          gap="8px"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Typography fontWeight="700" fontSize="16px">
-            {user.firstName} {user.lastName}
-          </Typography>
-          <IconButton onClick={onMenuOpen}>
-            <MoreVert />
-          </IconButton>
-          <Menu anchorEl={anchorEl} open={open} onClick={onMenuClose}>
-            {options.map(option => (
-              <MenuItem key={option.path} onClick={onMenuClick(option)}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box>
-        <Typography fontSize="12px" color="">
-          email: {user.email}
-        </Typography>
-        <Typography fontSize="12px" color="">
-          phone: {user.phone}
-        </Typography>
-        <Typography fontSize="12px" color="">
-          role: {user.role}
-        </Typography>
-        <Typography fontSize="12px" color="">
-          created at: {moment(user.createdAt).format('LLLL')}
-        </Typography>
-        {new Date(user.updatedAt) > new Date(user.createdAt) && (
+    <>
+      <DefaultContainer>
+        <Box width="100%" display="flex" flexDirection="column" alignItems="start" gap="8px">
+          <Box
+            width="100%"
+            mb="15px"
+            display="flex"
+            gap="8px"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Typography fontWeight="700" fontSize="16px">
+              {user.firstName} {user.lastName}
+            </Typography>
+            <IconButton onClick={onMenuOpen}>
+              <MoreVert />
+            </IconButton>
+            <Menu anchorEl={anchorEl} open={open} onClick={onMenuClose}>
+              {options.map(option => (
+                <MenuItem key={option.path} onClick={onMenuClick(option)}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
           <Typography fontSize="12px" color="">
-            last update: {moment(user.updatedAt).format('LLLL')}
+            email: {user.email}
           </Typography>
-        )}
-        <Box mt="30px">
-          {/**@ts-ignore */}
-          <Button type="danger">Delete account</Button>
+          <Typography fontSize="12px" color="">
+            phone: {user.phone}
+          </Typography>
+          <Typography fontSize="12px" color="">
+            role: {user.role}
+          </Typography>
+          <Typography fontSize="12px" color="">
+            created at: {moment(user.createdAt).format('LLLL')}
+          </Typography>
+          {new Date(user.updatedAt) > new Date(user.createdAt) && (
+            <Typography fontSize="12px" color="">
+              last update: {moment(user.updatedAt).format('LLLL')}
+            </Typography>
+          )}
+          <Box mt="30px">
+            {/**@ts-ignore */}
+            <Button onClick={onDeleteAccount} type="danger">
+              Delete account
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </DefaultContainer>
+      </DefaultContainer>
+      <Modal
+        title="Delete Account"
+        body="Are you sure do delete the user account?"
+        isActive={modals[ModalNames.CONFIRMATION]}
+        onCancel={() => hideModal(ModalNames.CONFIRMATION)}
+        onConfirm={() => console.log('submit')}
+      />
+    </>
   );
 };
 
