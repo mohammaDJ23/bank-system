bbf430dc415576e42fc844c9a755043c1a05b0a7
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-12-24 08:53:37 +0330

    adding some typo

--- clients/bank/src/apis/resetApi.ts
+++ clients/bank/src/apis/resetApi.ts
@@ -1,5 +1,18 @@
-import axios, { AxiosRequestConfig, CreateAxiosDefaults, AxiosInstance } from 'axios';
+import axios, {
+  AxiosRequestConfig,
+  CreateAxiosDefaults,
+  AxiosInstance,
+  AxiosError,
+  AxiosResponse,
+} from 'axios';
 import { getToken } from '../lib';
+
+export interface ErrorObj {
+  statusCode: number;
+  message: string;
+  timestamp: string;
+  path: string;
+}
 
 export class ResetApi {
   private axiosInstance: AxiosInstance;
@@ -10,16 +23,22 @@ export class ResetApi {
     this.axiosInstance = axios.create(config);
   }
 
-  async build<T extends unknown>(config: AxiosRequestConfig<T>) {
+  async build<T extends any = any, K extends any = any>(
+    config: AxiosRequestConfig<T>
+  ): Promise<AxiosResponse<T, K>> {
     try {
       return this.axiosInstance.request(config);
     } catch (error) {
-      throw error;
+      const err = error as AxiosError<ErrorObj>;
+      throw err;
     }
   }
 
-  static req<T>(req: AxiosRequestConfig<T>, config: CreateAxiosDefaults<T>) {
+  static req<T = any, K = any>(
+    req: AxiosRequestConfig<T>,
+    config: CreateAxiosDefaults<T>
+  ): Promise<AxiosResponse<T, K>> {
     const restApi = new ResetApi(config);
-    return restApi.build<T>(req);
+    return restApi.build<T, K>(req);
   }
 }
