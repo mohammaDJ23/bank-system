3b704d454e48981c7f14d0226962bf404b0beb51
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-01-03 08:20:29 +0330

    refactoring the UpdateUser component

---
+++ clients/bank/src/components/updateUser/Skeleton.tsx
@@ -0,0 +1,43 @@
+import { Box } from '@mui/material';
+import CustomSkeleton from '../Skeleton';
+
+const Skeleton = () => {
+  return (
+    <Box width="100%" display="flex" alignItems="start" gap="40px" flexDirection="column">
+      <Box width="100%" display="flex" alignItems="start" gap="15px" flexDirection="column">
+        <Box maxWidth="100px" width="100%" height="16px">
+          <CustomSkeleton width="100%" height="100%" />
+        </Box>
+        <Box width="100%" height="30px">
+          <CustomSkeleton width="100%" height="100%" />
+        </Box>
+      </Box>
+      <Box width="100%" display="flex" alignItems="start" gap="15px" flexDirection="column">
+        <Box maxWidth="100px" width="100%" height="16px">
+          <CustomSkeleton width="100%" height="100%" />
+        </Box>
+        <Box width="100%" height="30px">
+          <CustomSkeleton width="100%" height="100%" />
+        </Box>
+      </Box>
+      <Box width="100%" display="flex" alignItems="start" gap="15px" flexDirection="column">
+        <Box maxWidth="100px" width="100%" height="16px">
+          <CustomSkeleton width="100%" height="100%" />
+        </Box>
+        <Box width="100%" height="30px">
+          <CustomSkeleton width="100%" height="100%" />
+        </Box>
+      </Box>
+      <Box width="100%" display="flex" alignItems="start" gap="15px" flexDirection="column">
+        <Box maxWidth="100px" width="100%" height="16px">
+          <CustomSkeleton width="100%" height="100%" />
+        </Box>
+        <Box width="100%" height="30px">
+          <CustomSkeleton width="100%" height="100%" />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Skeleton;
