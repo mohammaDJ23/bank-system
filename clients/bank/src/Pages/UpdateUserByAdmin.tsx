957b655b61f353be963d78b759040fd4a81f54cd
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-01-14 22:12:07 +0330

    adding ProtectAdminRouteProvider and use it inside some related admin routes

--- clients/bank/src/Pages/UpdateUserByAdmin.tsx
+++ clients/bank/src/Pages/UpdateUserByAdmin.tsx
@@ -1,7 +1,15 @@
+import { useParams } from 'react-router-dom';
+import ProtectAdminRouteProvider from '../components/hoc/ProtectAdminRouteProvider';
 import UpdateUserByAdminContent from '../components/UpdateUserByAdmin';
 
 const UpdateUserByAdmin = () => {
-  return <UpdateUserByAdminContent />;
+  const params = useParams();
+
+  return (
+    <ProtectAdminRouteProvider path={`/bank/users/${params.id}`}>
+      <UpdateUserByAdminContent />
+    </ProtectAdminRouteProvider>
+  );
 };
 
 export default UpdateUserByAdmin;
