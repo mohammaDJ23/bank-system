1d26f2e7694ef8a7f558b572fb1ce68b5a695ae5
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-01-30 12:01:41 +0330

    adding the new ui to the forgot password

--- clients/auth/src/components/ForgotPassword.vue
+++ clients/auth/src/components/ForgotPassword.vue
@@ -1,5 +1,5 @@
 <template>
-  <Form :form-schema="formSchema" :rules="rules" show-login-button>
+  <!-- <Form :form-schema="formSchema" :rules="rules" show-login-button>
     <el-form-item class="w-100" label="Email" prop="email">
       <el-input
         :disabled="!!isFormProcessing"
@@ -10,12 +10,39 @@
         @input="cacheInput('email', $event)"
       />
     </el-form-item>
-  </Form>
+  </Form> -->
+  <Card title="Forgot password">
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        clearable
+        label="Email"
+        variant="underlined"
+        v-model="email"
+        type="email"
+        name="email"
+        @input="() => {}"
+      ></v-text-field>
+      <div class="d-flex align-center gap-2 flex-wrap">
+        <v-btn color="primary" class="text-lowercase" size="small" type="submit" @click="validate">
+          Send
+        </v-btn>
+        <v-btn
+          color="primary"
+          variant="outlined"
+          size="small"
+          class="text-lowercase"
+          @click="reset"
+        >
+          Back to login
+        </v-btn>
+      </div>
+    </v-form>
+  </Card>
 </template>
 
 <script setup>
 import { onMounted, reactive } from 'vue';
-import Form from './Form.vue';
+import Card from './Card.vue';
 import { isEmail, ForgotPassword } from '../lib';
 import { useForm, useFocus } from '../hooks';
 
