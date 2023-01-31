158f7afe7f283808ba46af6cdcec13cd0548ba57
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-01-31 13:34:21 +0330

    revising the usage of the api

--- clients/auth/src/apis/api.js
+++ clients/auth/src/apis/api.js
@@ -1,5 +1,51 @@
 import { Login, ForgotPassword, ResetPassword } from '../lib';
 
+class RootApi {
+  constructor(api = {}, config = {}) {
+    this.api = api;
+    this.config = config;
+  }
+}
+
+export class LoginApi extends RootApi {
+  constructor(data) {
+    super({
+      url: '/auth/login',
+      method: 'post',
+      data,
+      header: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
+
+export class ForgotPasswordApi extends RootApi {
+  constructor(data) {
+    super({
+      url: '/auth/forgot-password',
+      method: 'post',
+      data,
+      header: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
+
+export class ResetPasswordApi extends RootApi {
+  constructor(data) {
+    super({
+      url: '/auth/reset-password',
+      method: 'post',
+      data,
+      header: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
+
 export const apis = {
   [Login.name](data) {
     return {
