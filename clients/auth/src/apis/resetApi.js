3681793554d71701a9a4fc9dccd47490506fefc7
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-01-31 13:28:39 +0330

    adding the new Request api

--- clients/auth/src/apis/resetApi.js
+++ clients/auth/src/apis/resetApi.js
@@ -10,3 +10,23 @@ export class ResetApi {
     }
   }
 }
+
+export class Request {
+  constructor({ api = {}, config = {} }) {
+    this.api = api;
+    this.config = {
+      baseURL: process.env.AUTH_SERVICE,
+      timeout: 5000,
+      ...config,
+    };
+    this.axiosInstance = axios.create(this.config);
+  }
+
+  async build() {
+    try {
+      return this.axiosInstance.request(this.api);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
