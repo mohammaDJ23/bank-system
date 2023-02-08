fc5b64f1e74732b5ea06eac29b5db947c45d3354
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-02-08 14:46:47 +0330

    reuable the InitialInputsValue type

--- clients/bank/src/lib/decorators/defineVal.ts
+++ clients/bank/src/lib/decorators/defineVal.ts
@@ -1,19 +1,19 @@
 import { FormMetadataTypes } from './';
 
-type ValuesObj = Record<string, any>;
+type InitialInputsValue = Record<string, any>;
 
-export function getInitialInputsValue(target: any) {
+export function getInitialInputsValue(target: any): InitialInputsValue {
   return Reflect.getMetadata(FormMetadataTypes.VALUES, target) || {};
 }
 
-export function setInitialInputsValue(values: ValuesObj, target: any) {
+export function setInitialInputsValue(values: InitialInputsValue, target: any) {
   Reflect.defineMetadata(FormMetadataTypes.VALUES, values, target);
 }
 
 export function DefineVal() {
   return function (target: any, prop: string) {
     const value: any = Reflect.getMetadata('design:type', target, prop)();
-    const inputs: ValuesObj = getInitialInputsValue(target);
+    const inputs = getInitialInputsValue(target);
     const newInputs = Object.assign(inputs, { [prop]: value });
     setInitialInputsValue(newInputs, target);
   };
