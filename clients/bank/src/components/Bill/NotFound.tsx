f2c3fb370fc5e5de5acd90bbd85d10675ec9634d
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-03-26 16:02:44 +0430

    refactoring around the NotFound component of the bill component

--- clients/bank/src/components/Bill/NotFound.tsx
+++ clients/bank/src/components/Bill/NotFound.tsx
@@ -1,5 +1,4 @@
-import { Box, Typography } from '@mui/material';
-import { Button } from 'element-react';
+import { Box, Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { FC } from 'react';
 
@@ -7,18 +6,10 @@ const NotFound: FC = () => {
   const navigate = useNavigate();
 
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
-      gap="12px"
-      mt="20px"
-    >
+    <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" gap="12px" mt="20px">
       <Typography>Not found the bill</Typography>
-      {/**@ts-ignore */}
-      <Button onClick={() => navigate('/bank/bills')} type="primary">
-        Back to the bill list
+      <Button onClick={() => navigate('/bank/create-bill')} sx={{ textTransform: 'capitalize' }} variant="contained">
+        Create A New Bill
       </Button>
     </Box>
   );
