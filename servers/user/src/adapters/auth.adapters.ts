d251ba491541afb9e3c73f7a2a06aed5b36fa867
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-04-01 17:38:30 +0430

    adding the jwt socket io for auth adapter

--- servers/user/src/adapters/auth.adapters.ts
+++ servers/user/src/adapters/auth.adapters.ts
@@ -1,4 +1,5 @@
-import { UnauthorizedException } from '@nestjs/common';
+import { INestApplication, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { Socket, Server } from 'socket.io';
 import { User } from 'src/entities/user.entity';
@@ -12,18 +13,34 @@ export interface CustomSocket extends Socket {
 }
 
 export class AuthAdapter extends IoAdapter {
+  private jwtService: JwtService;
+
+  constructor(app: INestApplication) {
+    super(app);
+    app.resolve(JwtService).then((jwtService) => {
+      this.jwtService = jwtService;
+    });
+  }
+
   createIOServer(port: number, options?: any): Server {
     const server: Server = super.createIOServer(port, options);
+    const unAuthorizedError = new UnauthorizedException(
+      'No token is provided for socket.',
+    );
 
     server.use((socket: CustomSocket, next) => {
       const bearerToken = socket.handshake.headers.authorization;
 
       if (bearerToken) {
         const [_, token] = bearerToken.split(' ');
-        console.log(token);
-        next();
-      } else
-        next(new UnauthorizedException('No token is provided for socket.'));
+        this.jwtService
+          .verifyAsync(token)
+          .then((user) => {
+            socket.user = user;
+            next();
+          })
+          .catch((reason) => next(unAuthorizedError));
+      } else next(unAuthorizedError);
     });
 
     return server;
