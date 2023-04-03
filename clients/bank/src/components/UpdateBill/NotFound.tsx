d28512ce4e66c7ac3341e7b7b5d9c474438f8206
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-04-03 13:22:53 +0430

    refactoring around the UpdateBill component

--- clients/bank/src/components/UpdateBill/NotFound.tsx
+++ clients/bank/src/components/UpdateBill/NotFound.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { FC } from 'react';
+import { Pathes } from '../../lib';
 
 const NotFound: FC = () => {
   const navigate = useNavigate();
@@ -8,7 +9,12 @@ const NotFound: FC = () => {
   return (
     <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" gap="12px" mt="20px">
       <Typography>Not found the bill</Typography>
-      <Button onClick={() => navigate('/bank/bills')} sx={{ textTransform: 'capitalize' }} variant="contained">
+      <Button
+        onClick={() => navigate(Pathes.BILLS)}
+        sx={{ textTransform: 'capitalize' }}
+        variant="contained"
+        size="small"
+      >
         Navigate To The Bill List
       </Button>
     </Box>
