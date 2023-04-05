f58d7f2ee8402ebc684f2856828c22cd9b303a9e
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-04-05 18:03:55 +0430

    fixing the navigating the parent for child

--- clients/bank/src/lib/providers/RedirectionProvider.tsx
+++ clients/bank/src/lib/providers/RedirectionProvider.tsx
@@ -1,8 +1,20 @@
 import { Fragment, PropsWithChildren, FC, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+interface RedirectionDetailObj {
+  path: string;
+}
 
 const RedirectionProvider: FC<PropsWithChildren> = ({ children }) => {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    /**@ts-ignore */
+    window.addEventListener('parent-redirection', (event: CustomEvent<RedirectionDetailObj>) => {
+      navigate(event.detail.path);
+    });
+  }, []);
 
   useEffect(() => {
     const childRedirectionEvent = new CustomEvent('child-redirection', {
