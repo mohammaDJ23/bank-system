e24f09272d8665b2a88a038023f7a0bba887a86a
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-04-14 12:40:58 +0430

    revising the condition of the bank app

--- clients/container/src/components/Bank.tsx
+++ clients/container/src/components/Bank.tsx
@@ -1,13 +1,10 @@
 import { mount } from 'bank/BankApp';
 import { useInitialMicro } from '../hooks';
-import { isUserAuthenticated, pathes } from '../lib';
-import { Navigate } from 'react-router-dom';
 
 function Auth() {
   const { ref } = useInitialMicro(mount);
-  const isUserLoggedIn = isUserAuthenticated();
 
-  return isUserLoggedIn ? <div ref={ref} /> : <Navigate to={pathes.unauthorized} />;
+  return <div ref={ref} />;
 }
 
 export default Auth;
