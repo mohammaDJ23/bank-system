459e30aa65dbf0233d0d58587899ba5ede2f4cb1
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-04-28 10:13:57 +0430

    spliting the redirection logic to the RedirectionProvider component

--- clients/container/src/App.tsx
+++ clients/container/src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoadingFallback from './components/LoadingFallback';
+import RedirectionProvider from './components/RedirectionProvider';
 import UserServiceSocketProvider from './components/UserServiceSocketProvider';
 import { isUserAuthenticated, Pathes, routes } from './lib';
 import './lib/socket';
@@ -9,16 +10,18 @@ function App() {
   return (
     <UserServiceSocketProvider>
       <BrowserRouter>
-        <Routes>
-          {routes.map(route => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<Suspense fallback={<LoadingFallback />}>{route.element}</Suspense>}
-            />
-          ))}
-          <Route path="*" element={<Navigate to={isUserAuthenticated() ? Pathes.DASHBOARD : Pathes.LOGIN} />} />
-        </Routes>
+        <RedirectionProvider>
+          <Routes>
+            {routes.map(route => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={<Suspense fallback={<LoadingFallback />}>{route.element}</Suspense>}
+              />
+            ))}
+            <Route path="*" element={<Navigate to={isUserAuthenticated() ? Pathes.DASHBOARD : Pathes.LOGIN} />} />
+          </Routes>
+        </RedirectionProvider>
       </BrowserRouter>
     </UserServiceSocketProvider>
   );
--- clients/container/src/hooks/useInitialMicro.ts
+++ clients/container/src/hooks/useInitialMicro.ts
@@ -1,11 +1,7 @@
 import { useEffect, useRef } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 
 export function useInitialMicro(app: MicroApp) {
   const ref = useRef<HTMLDivElement | null>(null);
-  const navigate = useNavigate();
-  const location = useLocation();
-  const currentPathRef = useRef<string>(location.pathname);
 
   useEffect(() => {
     if (ref.current) {
@@ -13,25 +9,6 @@ export function useInitialMicro(app: MicroApp) {
       mount();
     }
   }, []);
-
-  useEffect(() => {
-    //@ts-ignore
-    window.addEventListener('child-redirection', (event: CustomEvent<RedirectionObj>) => {
-      if (event.detail.path !== currentPathRef.current) {
-        navigate(event.detail.path, { replace: true });
-      }
-    });
-  }, []);
-
-  useEffect(() => {
-    currentPathRef.current = location.pathname;
-    const event = new CustomEvent('parent-redirection', {
-      bubbles: true,
-      cancelable: true,
-      detail: { path: location.pathname },
-    });
-    window.dispatchEvent(event);
-  }, [location]);
 
   return { ref };
 }
