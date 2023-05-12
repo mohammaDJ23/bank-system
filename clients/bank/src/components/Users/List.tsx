752796458c79f103d0b7eecdb3d37074e9b91f21
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-05-12 09:25:11 +0430

    refactoring around the filters and the user list

--- clients/bank/src/components/Users/List.tsx
+++ clients/bank/src/components/Users/List.tsx
@@ -1,6 +1,6 @@
-import { FC, useCallback } from 'react';
+import { FC, useCallback, useEffect } from 'react';
 import {
-  List,
+  List as MuiList,
   ListItem,
   ListItemText,
   ListItemButton,
@@ -12,211 +12,255 @@ import {
 } from '@mui/material';
 import moment from 'moment';
 import { useNavigate } from 'react-router-dom';
-import { Pathes, UserObj, getUserRoleColor, UserListFilters, isoDate, getTime, UserRoles } from '../../lib';
+import { Pathes, UserObj, getUserRoleColor, UserListFilters, isoDate, getTime, UserRoles, UserList } from '../../lib';
 import CountBadge from '../CountBadge';
 import Pagination from '../Pagination';
 import { useAuth, useForm, usePaginationList, useRequest } from '../../hooks';
-import { UsersApi } from '../../apis';
+import { UsersApi, UsersApiConstructorType } from '../../apis';
 import Filter from '../Filter';
+import EmptyList from './EmptyList';
+import Skeleton from './Skeleton';
 
-interface UserListImportation {
-  listInstance: ReturnType<typeof usePaginationList<UserObj>>;
-  onPageChange: (newPage: number) => void;
-  onFilterSubmit: () => void;
-}
-
-const UserList: FC<UserListImportation> = ({ listInstance, onPageChange, onFilterSubmit }) => {
+const List: FC = () => {
   const navigate = useNavigate();
-  const userFiltersFormInstance = useForm(UserListFilters);
-  const form = userFiltersFormInstance.getForm();
-  const listInfo = listInstance.getFullInfo();
+  const { request, isInitialApiProcessing, isApiProcessing } = useRequest();
   const { getTokenInfo } = useAuth();
-  const { isApiProcessing } = useRequest();
-  const isUserApiProcessing = isApiProcessing(UsersApi);
+  const userListInstance = usePaginationList(UserList);
+  const userListFiltersFormInstance = useForm(UserListFilters);
+  const userListFiltersForm = userListFiltersFormInstance.getForm();
+  const userListInfo = userListInstance.getFullInfo();
+  const isInitialUsersApiProcessing = isInitialApiProcessing(UsersApi);
+  const isUsersApiProcessing = isApiProcessing(UsersApi);
   const userInfo = getTokenInfo();
   const isUserExist = !!userInfo;
 
-  const formSubmition = useCallback(() => {
-    userFiltersFormInstance.onSubmit(() => onFilterSubmit.call({}));
-  }, [userFiltersFormInstance, onFilterSubmit]);
+  const getUsersList = useCallback(
+    (options: Partial<UsersApiConstructorType> = {}) => {
+      const apiData = Object.assign(
+        { take: userListInfo.take, page: userListInfo.page, ...options },
+        userListFiltersForm
+      );
+      const userApi = new UsersApi<UserObj>(apiData);
+      userApi.setInitialApi(!!apiData.isInitialApi);
+
+      request<[UserObj[], number], UsersApiConstructorType>(userApi).then(response => {
+        const [list, total] = response.data;
+        userListInstance.insertNewList({ total, list, page: apiData.page });
+      });
+    },
+    [userListInfo, userListInstance, userListFiltersForm, request]
+  );
+
+  useEffect(() => {
+    getUsersList({ isInitialApi: true });
+  }, []);
+
+  const changePage = useCallback(
+    (newPage: number) => {
+      userListInstance.onPageChange(newPage);
+
+      if (userListInstance.isNewPageEqualToCurrentPage(newPage) || isUsersApiProcessing) return;
+
+      if (!userListInstance.isNewPageExist(newPage)) getUsersList({ page: newPage });
+    },
+    [userListInstance, isUsersApiProcessing, getUsersList]
+  );
+
+  const userListFilterFormSubmition = useCallback(() => {
+    userListFiltersFormInstance.onSubmit(() => {
+      const newPage = 1;
+      userListInstance.onPageChange(newPage);
+      getUsersList({ page: newPage });
+    });
+  }, [userListFiltersFormInstance, userListInstance, getUsersList]);
 
   return (
     <>
-      <List>
-        {listInfo.list.map((user, index) => (
-          <Card
-            key={index}
-            variant="outlined"
-            sx={{
-              my: '20px',
-              position: 'relative',
-              overflow: 'visible',
-              backgroundColor: isUserExist && user.id === userInfo.id && listInfo.list.length >= 2 ? '#F8F8F8' : '',
-            }}
-            onClick={() => navigate(Pathes.USER.replace(':id', user.id.toString()))}
-          >
-            <ListItemButton>
-              <ListItem disablePadding sx={{ my: '10px' }}>
-                <Box display="flex" flexDirection="column" alignItems="start" width="100%" gap="10px">
-                  <Box component="div">
-                    <ListItemText
-                      primaryTypographyProps={{ fontSize: '14px', mb: '10px' }}
-                      secondaryTypographyProps={{ fontSize: '12px' }}
-                      sx={{ margin: '0' }}
-                      primary={`${user.firstName} ${user.lastName}`}
-                      secondary={user.phone}
-                    />
-                  </Box>
-                  <Box
-                    component="div"
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="space-between"
-                    gap="10px"
-                    width="100%"
-                    flexWrap="wrap"
-                  >
-                    <Box
-                      component="div"
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="space-between"
-                      gap="10px"
-                      flexWrap="wrap"
-                    >
-                      <ListItemText
-                        sx={{
-                          flex: 'unset',
-                          width: '8px',
-                          height: '8px',
-                          backgroundColor: getUserRoleColor(user.role),
-                          borderRadius: '50%',
-                        }}
-                        secondary={<Box component="span"></Box>}
-                      />
-                      <ListItemText
-                        sx={{ flex: 'unset' }}
-                        secondaryTypographyProps={{ fontSize: '10px' }}
-                        secondary={user.role}
-                      />
+      {isInitialUsersApiProcessing || isUsersApiProcessing ? (
+        <Skeleton take={userListInfo.take} />
+      ) : userListInstance.isListEmpty() ? (
+        <EmptyList />
+      ) : (
+        <>
+          <MuiList>
+            {userListInfo.list.map((user, index) => (
+              <Card
+                key={index}
+                variant="outlined"
+                sx={{
+                  my: '20px',
+                  position: 'relative',
+                  overflow: 'visible',
+                  backgroundColor:
+                    isUserExist && user.id === userInfo.id && userListInfo.list.length >= 2 ? '#F8F8F8' : '',
+                }}
+                onClick={() => navigate(Pathes.USER.replace(':id', user.id.toString()))}
+              >
+                <ListItemButton>
+                  <ListItem disablePadding sx={{ my: '10px' }}>
+                    <Box display="flex" flexDirection="column" alignItems="start" width="100%" gap="10px">
+                      <Box component="div">
+                        <ListItemText
+                          primaryTypographyProps={{ fontSize: '14px', mb: '10px' }}
+                          secondaryTypographyProps={{ fontSize: '12px' }}
+                          sx={{ margin: '0' }}
+                          primary={`${user.firstName} ${user.lastName}`}
+                          secondary={user.phone}
+                        />
+                      </Box>
+                      <Box
+                        component="div"
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="space-between"
+                        gap="10px"
+                        width="100%"
+                        flexWrap="wrap"
+                      >
+                        <Box
+                          component="div"
+                          display="flex"
+                          alignItems="center"
+                          justifyContent="space-between"
+                          gap="10px"
+                          flexWrap="wrap"
+                        >
+                          <ListItemText
+                            sx={{
+                              flex: 'unset',
+                              width: '8px',
+                              height: '8px',
+                              backgroundColor: getUserRoleColor(user.role),
+                              borderRadius: '50%',
+                            }}
+                            secondary={<Box component="span"></Box>}
+                          />
+                          <ListItemText
+                            sx={{ flex: 'unset' }}
+                            secondaryTypographyProps={{ fontSize: '10px' }}
+                            secondary={user.role}
+                          />
+                        </Box>
+                        <ListItemText
+                          sx={{ flex: 'unset' }}
+                          secondaryTypographyProps={{ fontSize: '10px' }}
+                          secondary={
+                            new Date(user.updatedAt) > new Date(user.createdAt)
+                              ? `${moment(user.updatedAt).format('lll')} was updated`
+                              : `${moment(user.createdAt).format('lll')}`
+                          }
+                        />
+                      </Box>
                     </Box>
-                    <ListItemText
-                      sx={{ flex: 'unset' }}
-                      secondaryTypographyProps={{ fontSize: '10px' }}
-                      secondary={
-                        new Date(user.updatedAt) > new Date(user.createdAt)
-                          ? `${moment(user.updatedAt).format('lll')} was updated`
-                          : `${moment(user.createdAt).format('lll')}`
-                      }
-                    />
-                  </Box>
-                </Box>
-                <CountBadge take={listInfo.take} page={listInfo.page} index={index} />
-              </ListItem>
-            </ListItemButton>
-          </Card>
-        ))}
-      </List>
+                    <CountBadge take={userListInfo.take} page={userListInfo.page} index={index} />
+                  </ListItem>
+                </ListItemButton>
+              </Card>
+            ))}
+          </MuiList>
+          <Pagination page={userListInfo.page} count={userListInfo.count} onPageChange={changePage} />
+        </>
+      )}
 
-      <Pagination page={listInfo.page} count={listInfo.count} onPageChange={onPageChange} />
-
-      <Filter>
-        <Box
-          component="form"
-          noValidate
-          autoComplete="off"
-          display="flex"
-          flexDirection="column"
-          gap="20px"
-          onSubmit={event => {
-            event.preventDefault();
-            formSubmition();
-          }}
-        >
-          <TextField
-            label="Seach"
-            variant="standard"
-            type="text"
-            fullWidth
-            value={form.q}
-            onChange={event => userFiltersFormInstance.onChange('q', event.target.value)}
-            helperText={userFiltersFormInstance.getInputErrorMessage('q')}
-            error={userFiltersFormInstance.isInputInValid('q')}
-            name="q"
-            placeholder="first name, last name, phone"
-            disabled={isUserApiProcessing}
-          />
-          <Autocomplete
-            multiple
-            id="size-small-standard-multi"
-            size="small"
-            options={Object.values(UserRoles)}
-            getOptionLabel={(option: (typeof form.roles)[number]) => option}
-            onChange={(event, value) => userFiltersFormInstance.onChange('roles', value)}
-            value={form.roles}
-            renderInput={(params: any) => (
-              <TextField
-                {...params}
-                label="Roles"
-                variant="standard"
-                type="text"
-                error={userFiltersFormInstance.isInputInValid('roles')}
-                helperText={userFiltersFormInstance.getInputErrorMessage('roles')}
-                name="roles"
-              />
-            )}
-            disabled={isUserApiProcessing}
-          />
-
-          <TextField
-            label="From date"
-            type="date"
-            variant="standard"
-            value={form.fromDate ? isoDate(form.fromDate) : ''}
-            onChange={event => userFiltersFormInstance.onChange('fromDate', getTime(event.target.value))}
-            helperText={userFiltersFormInstance.getInputErrorMessage('fromDate')}
-            error={userFiltersFormInstance.isInputInValid('fromDate')}
-            InputLabelProps={{ shrink: true }}
-            name="fromDate"
-            disabled={isUserApiProcessing}
-          />
-          <TextField
-            label="To date"
-            type="date"
-            variant="standard"
-            value={form.toDate ? isoDate(form.toDate) : ''}
-            onChange={event => userFiltersFormInstance.onChange('toDate', getTime(event.target.value))}
-            helperText={userFiltersFormInstance.getInputErrorMessage('toDate')}
-            error={userFiltersFormInstance.isInputInValid('toDate')}
-            InputLabelProps={{ shrink: true }}
-            name="toDate"
-            disabled={isUserApiProcessing}
-          />
-          <Box component="div" display="flex" alignItems="center" gap="10px" marginTop="20px">
-            <Button
-              disabled={isUserApiProcessing || !userFiltersFormInstance.isFormValid()}
-              variant="contained"
+      {!isInitialUsersApiProcessing && (
+        <Filter>
+          <Box
+            component="form"
+            noValidate
+            autoComplete="off"
+            display="flex"
+            flexDirection="column"
+            gap="20px"
+            onSubmit={event => {
+              event.preventDefault();
+              userListFilterFormSubmition();
+            }}
+          >
+            <TextField
+              label="Seach"
+              variant="standard"
+              type="text"
+              fullWidth
+              value={userListFiltersForm.q}
+              onChange={event => userListFiltersFormInstance.onChange('q', event.target.value)}
+              helperText={userListFiltersFormInstance.getInputErrorMessage('q')}
+              error={userListFiltersFormInstance.isInputInValid('q')}
+              name="q"
+              placeholder="first name, last name, phone"
+              disabled={isUsersApiProcessing}
+            />
+            <Autocomplete
+              multiple
+              id="size-small-standard-multi"
               size="small"
-              type="submit"
-              sx={{ textTransform: 'capitalize' }}
-            >
-              Filter
-            </Button>
-            <Button
-              disabled={false}
-              variant="outlined"
-              size="small"
-              type="button"
-              sx={{ textTransform: 'capitalize' }}
-              onClick={() => userFiltersFormInstance.resetForm()}
-            >
-              Reset
-            </Button>
+              options={Object.values(UserRoles)}
+              getOptionLabel={(option: (typeof userListFiltersForm.roles)[number]) => option}
+              onChange={(event, value) => userListFiltersFormInstance.onChange('roles', value)}
+              value={userListFiltersForm.roles}
+              renderInput={(params: any) => (
+                <TextField
+                  {...params}
+                  label="Roles"
+                  variant="standard"
+                  type="text"
+                  error={userListFiltersFormInstance.isInputInValid('roles')}
+                  helperText={userListFiltersFormInstance.getInputErrorMessage('roles')}
+                  name="roles"
+                />
+              )}
+              disabled={isUsersApiProcessing}
+            />
+
+            <TextField
+              label="From date"
+              type="date"
+              variant="standard"
+              value={userListFiltersForm.fromDate ? isoDate(userListFiltersForm.fromDate) : ''}
+              onChange={event => userListFiltersFormInstance.onChange('fromDate', getTime(event.target.value))}
+              helperText={userListFiltersFormInstance.getInputErrorMessage('fromDate')}
+              error={userListFiltersFormInstance.isInputInValid('fromDate')}
+              InputLabelProps={{ shrink: true }}
+              name="fromDate"
+              disabled={isUsersApiProcessing}
+            />
+            <TextField
+              label="To date"
+              type="date"
+              variant="standard"
+              value={userListFiltersForm.toDate ? isoDate(userListFiltersForm.toDate) : ''}
+              onChange={event => userListFiltersFormInstance.onChange('toDate', getTime(event.target.value))}
+              helperText={userListFiltersFormInstance.getInputErrorMessage('toDate')}
+              error={userListFiltersFormInstance.isInputInValid('toDate')}
+              InputLabelProps={{ shrink: true }}
+              name="toDate"
+              disabled={isUsersApiProcessing}
+            />
+            <Box component="div" display="flex" alignItems="center" gap="10px" marginTop="20px">
+              <Button
+                disabled={isUsersApiProcessing || !userListFiltersFormInstance.isFormValid()}
+                variant="contained"
+                size="small"
+                type="submit"
+                sx={{ textTransform: 'capitalize' }}
+              >
+                Filter
+              </Button>
+              <Button
+                disabled={false}
+                variant="outlined"
+                size="small"
+                type="button"
+                sx={{ textTransform: 'capitalize' }}
+                onClick={() => userListFiltersFormInstance.resetForm()}
+              >
+                Reset
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </Filter>
+        </Filter>
+      )}
     </>
   );
 };
 
-export default UserList;
+export default List;
--- clients/bank/src/components/Users/index.tsx
+++ clients/bank/src/components/Users/index.tsx
@@ -1,77 +1,21 @@
 import ListContainer from '../../layout/ListContainer';
-import { Pathes, UserList, UserListFilters, UserObj } from '../../lib';
-import EmptyList from './EmptyList';
+import { getDynamicPath, Pathes } from '../../lib';
 import List from './List';
-import Skeleton from './Skeleton';
-import { FC, useCallback, useEffect } from 'react';
-import { useForm, usePaginationList, useRequest } from '../../hooks';
-import { UsersApi, UsersApiConstructorType } from '../../apis';
+import { FC } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
 const UsersContent: FC = () => {
   const location = useLocation();
-  const userListFiltersInstance = useForm(UserListFilters);
-  const { request, isInitialApiProcessing, isApiProcessing } = useRequest();
-  const userListInstance = usePaginationList(UserList);
-  const userListInfo = userListInstance.getFullInfo();
-  const isInitialUsersApiProcessing = isInitialApiProcessing(UsersApi);
-  const isUsersApiProcessing = isApiProcessing(UsersApi);
   const previousUserId: string | undefined = location.state?.previousUserId;
-  const isPreviousUserExist = !!previousUserId;
+  const isPreviousUserIdExist = !!previousUserId;
 
-  const getUsersList = useCallback(
-    (options: Partial<UsersApiConstructorType> = {}) => {
-      const userFilters = userListFiltersInstance.getForm();
-      const apiData = Object.assign({ take: userListInfo.take, page: userListInfo.page, ...options }, userFilters);
-      const userApi = new UsersApi<UserObj>(apiData);
-
-      if (apiData.isInitialApi) {
-        userApi.setInitialApi();
-      }
-
-      request<[UserObj[], number], UsersApiConstructorType>(userApi).then(response => {
-        const [list, total] = response.data;
-        userListInstance.insertNewList({ total, list, page: apiData.page });
-      });
-    },
-    [userListInfo, userListInstance, userListFiltersInstance, request]
-  );
-
-  useEffect(() => {
-    if (isPreviousUserExist) return;
-    getUsersList({ isInitialApi: true });
-  }, []);
-
-  const changePage = useCallback(
-    (newPage: number) => {
-      userListInstance.onPageChange(newPage);
-
-      if (userListInstance.isNewPageEqualToCurrentPage(newPage) || isUsersApiProcessing) return;
-
-      if (!userListInstance.isNewPageExist(newPage)) getUsersList({ page: newPage });
-    },
-    [userListInstance, isUsersApiProcessing, getUsersList]
-  );
-
-  const onFilterSubmit = useCallback(() => {
-    const newPage = 1;
-    userListInstance.onPageChange(newPage);
-    getUsersList({ page: newPage });
-  }, [getUsersList, userListInstance]);
-
-  if (isPreviousUserExist) {
-    return <Navigate to={Pathes.USER.replace(':id', previousUserId)} />;
+  if (isPreviousUserIdExist) {
+    return <Navigate to={getDynamicPath(Pathes.USER, { id: previousUserId })} />;
   }
 
   return (
     <ListContainer>
-      {isInitialUsersApiProcessing || isUsersApiProcessing ? (
-        <Skeleton take={userListInfo.take} />
-      ) : userListInstance.isListEmpty() ? (
-        <EmptyList />
-      ) : (
-        <List listInstance={userListInstance} onPageChange={changePage} onFilterSubmit={onFilterSubmit} />
-      )}
+      <List />
     </ListContainer>
   );
 };
