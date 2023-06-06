ca1baebefa9ec69b47ee67e1bbd46b4606c0eaf2
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2023-06-06 23:00:58 +0330

    fixing the bug related to the color of the user card

--- clients/bank/src/components/shared/UserCard.tsx
+++ clients/bank/src/components/shared/UserCard.tsx
@@ -26,7 +26,7 @@ const UserCard: FC<UserCardImportion> = ({ user, index, listInfo }) => {
         my: '20px',
         position: 'relative',
         overflow: 'visible',
-        backgroundColor: isUserExist && user.id === userInfo.id && listInfo.list.length >= 2 ? '#F8F8F8' : '',
+        backgroundColor: isUserExist && user.id === userInfo.id && listInfo.total >= 2 ? '#F8F8F8' : '',
       }}
       onClick={() => navigate(getDynamicPath(Pathes.USER, { id: user.id.toString() }))}
     >
