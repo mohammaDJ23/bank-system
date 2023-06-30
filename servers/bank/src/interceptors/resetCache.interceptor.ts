1e96321f524f6e95efd93b88d347749625342f8c
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-06-30 21:01:45 +0330

    adding the reset cache logic

--- servers/bank/src/interceptors/resetCache.interceptor.ts
+++ servers/bank/src/interceptors/resetCache.interceptor.ts
@@ -9,6 +9,7 @@ import {
 import { Cache } from 'cache-manager';
 import { Observable, map } from 'rxjs';
 import { BaseCacheService } from 'src/services';
+import { CacheKeyTypes } from 'src/types';
 
 @Injectable()
 export class ResetCacheInterceptor implements NestInterceptor {
@@ -23,6 +24,41 @@ export class ResetCacheInterceptor implements NestInterceptor {
   ): Observable<any> | Promise<Observable<any>> {
     return next.handle().pipe(
       map(async (data: any) => {
+        const cachedKeys = await this.cacheService.store.keys();
+        const resetCacheKeys = this.baseCahceService.getResetCacheKeys(context);
+        const findedCachedKeys: Promise<void>[] = [];
+        let currentCachedKeySign: string | null = null;
+        let currentCachedKey: string | null = null;
+
+        for (const cachedKeySign of cachedKeys) {
+          [currentCachedKeySign] = cachedKeySign.split('@');
+          [, currentCachedKey] = currentCachedKeySign.split('.');
+
+          secondLoop: for (const resetCacheKey of resetCacheKeys) {
+            if (currentCachedKey === resetCacheKey) {
+              const cachedKeySignType =
+                this.baseCahceService.getCacheKeySignType(currentCachedKeySign);
+              const resetCacheKeySign =
+                cachedKeySignType === CacheKeyTypes.PRIVATE
+                  ? this.baseCahceService.getPrivateCacheKeySign(
+                      context,
+                      resetCacheKey,
+                    )
+                  : this.baseCahceService.getPublicCacheKeySign(
+                      context,
+                      resetCacheKey,
+                    );
+              if (resetCacheKeySign === cachedKeySign) {
+                findedCachedKeys.push(this.cacheService.del(cachedKeySign));
+              }
+              break secondLoop;
+            }
+          }
+        }
+
+        console.log(findedCachedKeys);
+
+        // if (findedCachedKeys.length) Promise.all(findedCachedKeys);
         return data;
       }),
     );
--- servers/bank/src/services/baseCache.service.ts
+++ servers/bank/src/services/baseCache.service.ts
@@ -65,18 +65,37 @@ export class BaseCacheService {
     return /^[a-zA-Z._-]+\.+\d+$/.test(cacheKey);
   }
 
-  getCacheKeySign(context: ExecutionContext): string {
+  getCacheKeySignType(cacheKey: string): CacheKeyTypes {
+    if (this.isPrivateCacheKey(cacheKey)) return CacheKeyTypes.PRIVATE;
+    else if (this.isPublicCacheKey(cacheKey)) return CacheKeyTypes.PUBLIC;
+    else
+      throw new InternalServerErrorException(
+        'The type of the cache is invalid',
+      );
+  }
+
+  getPrivateCacheKeySign(context: ExecutionContext, cacheKey: CacheKeys) {
     const request = getRequest(context);
     const currentUser = getCurrentUser(context);
     const originalUrl = request.originalUrl;
     const userServiceId = currentUser.userServiceId;
+    return `${userServiceId}.${cacheKey}.${process.env.PORT}@${originalUrl}`;
+  }
+
+  getPublicCacheKeySign(context: ExecutionContext, cacheKey: CacheKeys) {
+    const request = getRequest(context);
+    const originalUrl = request.originalUrl;
+    return `${cacheKey}.${process.env.PORT}@${originalUrl}`;
+  }
+
+  getCacheKeySign(context: ExecutionContext): string {
     const cacheKey = this.getCacheKey(context);
     const cacheKeyType = this.getCacheKeyType(context);
     switch (cacheKeyType) {
       case CacheKeyTypes.PRIVATE:
-        return `${userServiceId}.${cacheKey}.${process.env.PORT}@${originalUrl}`;
+        return this.getPrivateCacheKeySign(context, cacheKey);
       case CacheKeyTypes.PUBLIC:
-        return `${cacheKey}.${process.env.PORT}@${originalUrl}`;
+        return this.getPublicCacheKeySign(context, cacheKey);
       default:
         throw new InternalServerErrorException(
           'The type of the cache is invalid',
