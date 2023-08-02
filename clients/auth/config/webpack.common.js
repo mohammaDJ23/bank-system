41bf9b6d98f5510613204204d1a5f02361bc0ba8
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-08-02 07:47:54 +0330

    bank to the dev mode and using env as public path

--- clients/auth/config/webpack.common.js
+++ clients/auth/config/webpack.common.js
@@ -84,7 +84,7 @@ module.exports = {
     }),
   ],
   output: {
-    publicPath: 'http://localhost:3005/',
+    publicPath: process.env.PUBLIC_PATH,
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, '../dist'),
     clean: true,
