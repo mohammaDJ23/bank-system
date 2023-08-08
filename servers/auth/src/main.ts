9567abe1c4c2bf5977bd2eb2067004169f47a395
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-08-08 18:50:00 +0330

    removing the dummy change

--- servers/auth/src/main.ts
+++ servers/auth/src/main.ts
@@ -7,8 +7,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 require('dotenv').config();
 
-console.log('run');
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
